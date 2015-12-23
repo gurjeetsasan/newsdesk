a53a4f4199482fb7575a299e00f8473f5c5295d7
Gurjeet singh <gurjeet.sasan@gmail.com>
2015-12-23 15:16:44 +0100

    Modified Files

--- application/views/admin/user_list.php
+++ application/views/admin/user_list.php
@@ -1,6 +1,19 @@
 <div class="row">
 	<div class="col-xs-12">
   		<div class="table-responsive">
+  			<?php if(isset( $message) ): ?>			
+				<?php if( $message_type == 'success' ): ?>			
+					<div class="alert bg-success alert-message" role="alert">
+						<svg class="glyph stroked checkmark"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#stroked-checkmark"></use></svg> <?php echo $message;?> <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+					</div>				
+				<?php endif; ?>
+				<?php if( $message_type == 'error' ): ?>			
+					<div class="alert bg-danger alert-message" role="alert">
+						<svg class="glyph stroked cancel"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#stroked-cancel"></use></svg> <?php echo $message;?> <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+					</div>
+				<?php endif; ?>
+			<?php endif; ?>
+			
     		<table class="table table-bordered table-hover normal-text-wrap">          
       			<thead>
 			    	<tr>
@@ -17,10 +30,13 @@
 			      		foreach ($user_data as $data ) {
 			      			echo "<tr>";
 			      				echo "<td class='mob-hide-section'>".$data->userId."</td>";
-			      				echo "<td>".$data->email."</td>";
+			      				
+			      				echo "<td> <a href='".base_url()."index.php/admin/view_publication/".$data->userId."'>".$data->email."</a> </td>";
+
 			      				echo "<td class='mob-hide-section'>".$data->password."</td>";
 			      				echo "<td class='mob-hide-section'>".$data->language."</td>";
-			      				echo '<td>
+			      				echo '<td>			      				
+			      							<a href="'.base_url().'index.php/admin/view_publication/'.$data->userId.'" class="edit-btn-ctm"><i class="fa fa-eye"></i></a>&nbsp;&nbsp;
 			      							<a href="'.base_url().'index.php/admin/update_user/'.$data->userId.'" class="edit-btn-ctm"><i class="fa fa-pencil-square-o"></i></a>&nbsp;
 			      							&nbsp;<a onclick="return confirm(\'Are you sure you want to Remove User?\');" href="'.base_url().'index.php/admin/user_list/delete/'.$data->userId.'" class="delete-btn-ctm"><i class="fa fa-trash"></i></a> &nbsp;
 										</td>';
--- template/js/custom.js
+++ template/js/custom.js
@@ -1,1 +1,4 @@
 /* add custom js here. */
+jQuery( document ).ready(function() {
+    jQuery('.glyphicon-remove').on('click', function(){ jQuery('.alert').fadeOut(1000); return false; });    
+});
